218a911751487c405eed701ac4940eb89b154f99
thiagocasti <thiagocasti123@gmail.com>
2024-03-29 22:41:54 -0300

    se mejoro el ejercicio 3

--- ejercicios-carpeta/ej-3.c
+++ ejercicios-carpeta/ej-3.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]){
     int num = atoi(argv[1]);
 
-    char *esMultiplo = (num % 2 == 0) ? "es multiplo de 2\n" : (num % 3 == 0) ? "Es multiplo de 3\n" : "no es multiplo ni de 2 ni 3";
+    char *esMultiplo = (num % 2 == 0 && num % 3 == 0) ? "es multiplo de 2 y de 3\n" : (num % 2 == 0) ? "Es multiplo de 2\n" : (num % 3 == 0) ? "Es multpilo de 3\n" : "no es multiplo ni de 2 ni de 3\n";
     printf("%s", esMultiplo);
     return 0;
 }
