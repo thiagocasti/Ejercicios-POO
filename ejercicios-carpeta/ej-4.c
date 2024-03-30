de6c2d24743ad0b281a757e8bc17005da55af4c6
thiagocasti <thiagocasti123@gmail.com>
2024-03-29 22:53:43 -0300

    se mejoro el ejercicio 4

--- ejercicios-carpeta/ej-4.c
+++ ejercicios-carpeta/ej-4.c
@@ -5,15 +5,8 @@ int main(int argc, char *argv[]){
     int n1 = atoi(argv[1]);
     int n2 = atoi(argv[2]);
     int n3 = atoi(argv[3]);
-
-    if(n1 > n2){
-        if(n1 > n3){
-            printf("El primer numero es el mayor\n");
-        }
-        }else if(n2 > n3){
-            printf("El numero 2 es el mayor\n");
-        }else{
-            printf("El numero 3 es el mayor\n");
-        }
+    
+    char *esPositivo = (n1 == n2 && n1 == n3) ? "Son iguales" : (n1 > n2 && n1 > n3) ? "El primer numero es el mayor" : (n2 > n3) ? "El segundo numero es el mayor" : "El tercer numero es el mayor"; 
+    printf("%s", esPositivo);
     return 0;
 }
