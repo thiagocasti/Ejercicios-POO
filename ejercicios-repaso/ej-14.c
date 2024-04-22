09e4bbd30ce5b0ec69c680967e215d81266fa13d
thiagocasti <thiagocasti123@gmail.com>
2024-04-22 00:36:50 -0300

    Se añadio el ejercicio 14

--- ejercicios-repaso/ej-14.c
+++ ejercicios-repaso/ej-14.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+int main(void){
+    int n;
+
+    printf("Ingrese un numero para convertirlo en su valor absoluto: ");
+    scanf("%d", &n);
+
+    int numeroAbsoulto = (n < 0) ? (n * -2) + n : n;
+
+    printf("El valor absoluto es: %d", numeroAbsoulto);
+}
