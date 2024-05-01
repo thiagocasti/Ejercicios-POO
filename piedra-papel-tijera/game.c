487581bc23eeaa7ed7b8d30a831e7e23cf2851ee
thiagocasti <thiagocasti123@gmail.com>
2024-05-01 11:47:09 -0300

    Seactualizo el condicional, se agregaron colores a los mensajes de roda, perdio, gano y empato y se agrego un mensaje para que el usuario pueda ver que eligio la computadora en esa ronda especifica

--- piedra-papel-tijera/game.c
+++ piedra-papel-tijera/game.c
@@ -4,6 +4,7 @@
 
 
 int main(void){
+    char *elecciones[] = {"no puede ser 0", "piedra", "papel", "tijera"};
     int userOption;
     int puntosComputadora = 0;
     int puntosUsuario = 0;
@@ -14,19 +15,18 @@ int main(void){
 
     for(int i = 1; i <= 5; i++){
         for(int j = 1; j <= 3; j++){
-                printf("\nRonda: %d Jugada: %d\n", i, j);
+                printf("\033[1;33m\nRonda: %d Jugada: %d\n\033[0m", i, j);
                 printf("Elija a que le juega(Ingrese 1 para piedra, 2 para papel y 3 para tijera): ");
                 scanf("%d", &userOption);
 
                 int computerElection = rand() % 3 + 1;
-
+                printf("\nla computadora eligio: %s\n", elecciones[computerElection]);
                 if(computerElection == userOption){
-                    puntosUsuario == puntosComputadora;
                      printf("Usted empato!\n");
     
-                }else if((userOption == 1 && computerElection == 2) ||
-                (userOption == 2 && computerElection == 3) ||
-                (userOption == 3 && computerElection == 1)){
+                }else if((userOption == 1 && computerElection == 3) ||
+                (userOption == 2 && computerElection == 1) ||
+                (userOption == 3 && computerElection == 2)){
                     printf("Usted gano!\n");
                     puntosUsuario++;
                 
@@ -37,10 +37,10 @@ int main(void){
                 }
                  printf("Puntos usted: %d\n", puntosUsuario);
                  printf("Puntos computadora: %d\n", puntosComputadora);    
-                }
         }
 
-        char *quienGano = (puntosUsuario == puntosComputadora) ? "'\nUsted empato\n" : (puntosUsuario > puntosComputadora) ? "\nUsted gano la partida\n" : "\nLa computadora gano la partida!\n";
+                }
+        char *quienGano = (puntosUsuario == puntosComputadora) ? "\033[0;32m\nHan empatado!\n" : (puntosUsuario > puntosComputadora) ? "\033[0;32m\nGanaste la partida!\n" : "\033[0;31m\nLa computadora gano la partida!\n";
         printf("%s", quienGano);
             return 0;
     }
