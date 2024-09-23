38d738e5b52789d3ba67c1a13042f7606e1c9fb9
Thiago Castiñeira <130512347+thiagocasti@users.noreply.github.com>
2024-09-22 23:47:35 -0300

    Create ejercicios_24.java

---
+++ ejercicios-java-propuestos/ejercicios_propuestos/src/ejercicios_propuestos/ejercicios_24.java
@@ -0,0 +1,11 @@
+public class ejercicios_24
+{
+    public static void main(String[] args) {
+        String frase = "La lluvia en Sevilla es una maravilla";
+        
+        // Reemplazamos todas las 'a' por 'e'
+        String fraseModificada = frase.replace('a', 'e');
+        
+        System.out.println("Frase modificada: " + fraseModificada);
+    }
+}
