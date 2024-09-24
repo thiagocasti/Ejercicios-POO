0724e951e4a1eb92c7c4ab06e3f7c37284193bb3
Thiago Castiñeira <130512347+thiagocasti@users.noreply.github.com>
2024-09-24 14:30:34 -0300

    Create ejercicio_26.java

---
+++ ejercicios-java-propuestos/ejercicios_propuestos/src/ejercicios_propuestos/ejercicio_26.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        
+        // Solicitar al usuario un número mayor que 1
+        do {
+            System.out.print("Introduce un número mayor que 1: ");
+            numero = scanner.nextInt();
+        } while (numero <= 1);
+        
+        int suma = 0;
+        
+        // Realizar la suma del 1 hasta el número indicado
+        for (int i = 1; i <= numero; i++) {
+            suma += i;
+        }
+        
+        // Muestra el resultado
+        System.out.println("La suma de los números de 1 a " + numero + " es: " + suma);
+        
+        scanner.close();
+    }
+}
